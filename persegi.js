c3b5350608b909e96fa126e11070b232b739620a
arkanazmi2025-a11y <arkanazmi2025@gmail.com>
2026-04-23 10:58:34 +0700

    persegi commit

---
+++ persegi.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')({sigint: true});
+let sisi = Number(prompt('masukan sisi: '));
+let luas = sisi * sisi;
+let keliling = 4 * sisi;
+
+console.log("===============================");
+console.log("              hasil            ");
+console.log("===============================");
+
+console.log(`luas persegi : ${ luas.toFixed(2) } cm`);
+console.log(`keliling persegi : ${ keliling.toFixed(2) } cm`);
+console.log(
+`luas persegi\t\t:  ${ luas.toFixed(2) } cm2
+    keliling persegi\t:  ${ keliling.toFixed(2) } cm`
+);
+232323
